9f95af9af37d2a6abdbd7a667c8b6c8c83df1296
Shrey Paharia <shreypaharia@gmail.com>
2024-10-05 15:25:16 +0530

    fix: update uniswap dependency

--- src/SwapProtocol.sol
+++ src/SwapProtocol.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap-periphery/interfaces/ISwapRouter.sol";
 import "@permit2/contracts/interfaces/IPermit2.sol";
 
 contract SwapProtocol is Ownable {
